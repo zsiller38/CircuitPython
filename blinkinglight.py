4ca00496a9966e2353731699e2c46dc6cd533f28
zsiller38 <zsiller38@charlottesvilleschools.org>
2022-09-12 15:41:00 -0400

    light crazy

--- blinkinglight.py
+++ blinkinglight.py
@@ -1,5 +1,7 @@
 import board
 import neopixel
+import time
+import math
 
 dot = neopixel.NeoPixel(board.NEOPIXEL, 1)
 dot.brightness = 0.5 
@@ -7,4 +9,11 @@ dot.brightness = 0.5
 print("Make it red!")
 
 while True:
-    dot.fill((0, 0, 255))
+    dot.fill((0, 255, 0))
+    time.sleep(.5)
+    dot.fill((0, 0, 0))
+    time.sleep(.5)
+    dot.fill((255, 0, 0))
+    time.sleep(.5)
+    dot.fill((0, 0, 0))
+    time.sleep(.5)
