ab533cba5c5d12ff507eec8b9ce1ae9dfbfdea71
zsiller38 <zsiller38@charlottesvilleschools.org>
2022-09-13 15:08:45 -0400

    comment

--- servocode.py
+++ servocode.py
@@ -4,14 +4,14 @@ import board
 import pwmio
 from adafruit_motor import servo
 
-
+#sets pin number
 pwm = pwmio.PWMOut(board.D9, duty_cycle=2 ** 15, frequency=50)
 
 
 my_servo = servo.Servo(pwm)
 
 while True:
-    for angle in range(0, 180, 5):
+    for angle in range(0, 180, 5): #moves servo then moves it back
         my_servo.angle = angle
         time.sleep(0.05)
     for angle in range(180, 0, -5): 
