b14e71855931b691be0efacc597f14cc6b76e7a0
zsiller38 <zsiller38@charlottesvilleschools.org>
2022-09-13 15:10:52 -0400

    comments

--- blinkinglight.py
+++ blinkinglight.py
@@ -9,7 +9,7 @@ dot.brightness = 0.5
 print("Make it red!")
 
 while True:
-    dot.fill((0, 255, 0))
+    dot.fill((0, 255, 0)) #tuuns light a color then turn it off and changes to a new color.
     time.sleep(.5)
     dot.fill((0, 0, 0))
     time.sleep(.5)
