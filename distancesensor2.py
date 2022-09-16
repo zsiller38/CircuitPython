1c346b8f950e92be26432a76f921bf3e009ce886
zsiller38 <zsiller38@charlottesvilleschools.org>
2022-09-16 15:36:15 -0400

    hi

--- distancesensor2.py
+++ distancesensor2.py
@@ -1,29 +1,39 @@
 # SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
 # SPDX-License-Identifier: MIT
-
+import digitalio
+import simpleio
 import time
 import board
 import adafruit_hcsr04
-import neopixel
+import neopixel                       
+from board import *
 
-dot=neopixel.NeoPixel(board.NEOPIXEL,1)
-dot.brightness = .01
-sonar = adafruit_hcsr04.HCSR04(trigger_pin=board.D5, echo_pin=board.D6)
+sonar = adafruit_hcsr04.HCSR04(trigger_pin=board.D3, echo_pin=board.D2)
+led = neopixel.NeoPixel(board.NEOPIXEL, 1)#connecting the neopixel on the board to the code
+led.brightness = 0.1
+1  #setting the brightness of the light, from 0-1 brightness
+ledOutput = 0
+Red = 0
+Green = 0
+Blue = 0
 
 while True:
-
+    
     try:
-        print((sonar.distance,))
-        cm=sonar.distance
-        
+        cm = sonar.distance
+        print((sonar.distance, Red, Green, Blue))
+        time.sleep(0.01)
         if cm < 5:
-            dot.fill((255,0,0))
-        elif 20 > cm > 5:
-            dot.fill((0,0,255))
+            led.fill((255, 0, 0))#setting the color with RGB values
+        elif cm > 5 and cm < 10:
+            Green = 0
+            Red = simpleio.map_range(cm, 5.1, 10, 255, 0)
+            Blue = simpleio.map_range(Red, 0, 255, 255, 0)
+            led.fill((Red, Green, Blue))
         else:
-            dot.fill((0,255,0))
-        
-
+            Blue = simpleio.map_range(cm, 10.1, 20, 255, 0)
+            Green = simpleio.map_range(Blue, 0, 255, 255, 0)
+            led.fill((0, Green, Blue))#setting the color with RGB values
     except RuntimeError:
         print("Retrying!")
-    time.sleep(0.1)
+    time.sleep(0.01)
