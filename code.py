c9343f5f128ac70bcb5897c10672b098b8c43193
zsiller38 <zsiller38@charlottesvilleschools.org>
2022-09-27 15:19:06 -0400

    hi

--- code.py
+++ code.py
@@ -1,5 +1,8 @@
+#zachary siller
+#9/13/2022
+#goal print hello world on serial monitor
 from time import sleep
 
 while True:
-    print("Hello world!")
+    print("Hello world!") #prints hello world
     sleep(4)
