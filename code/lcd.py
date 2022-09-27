72a68c88d3891f48c8b9d7af7ba2d32e7d874f1f
zsiller38 <zsiller38@charlottesvilleschools.org>
2022-09-27 15:28:53 -0400

    h

--- code/lcd.py
+++ code/lcd.py
@@ -1,25 +1,3 @@
-<<<<<<< HEAD:lcd.py
-import board
-from lcd.lcd import LCD
-from lcd.i2c_pcf8574_interface import I2CPCF8574Interface
-
-# get and i2c object
-i2c = board.I2C()
-
-# some LCDs are 0x3f... some are 0x27.
-lcd = LCD(I2CPCF8574Interface(i2c, 0x27), num_rows=2, num_cols=16)
-
-lcd.print("Hello, Engineer!")
-=======
-import board
-from lcd.lcd import LCD
-from lcd.i2c_pcf8574_interface import I2CPCF8574Interface
-
-# get and i2c object
-i2c = board.I2C()
-
-# some LCDs are 0x3f... some are 0x27.
-lcd = LCD(I2CPCF8574Interface(i2c, 0x27), num_rows=2, num_cols=16)
-
-lcd.print("Hello, Engineer!")
->>>>>>> c81f96bfd7f9bf0d48f8762597c836f2afff15a8:code/lcd.py
+#zachary siller
+#9/27/2022
+#display counter on LCD display and use two buttons to count up and down
