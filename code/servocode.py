9a1f67b52d65a88b6c66f1a792c2f3c2e0b8f30c
zsiller38 <zsiller38@charlottesvilleschools.org>
2022-10-11 15:49:45 -0400

    hi

--- code/servocode.py
+++ code/servocode.py
@@ -2,31 +2,30 @@
 # 9/29/2022
 # hit button to make a servo turn
 
-"""CircuitPython Essentials Servo standard servo example"""
-from digitalio import DigitalInOut, Direction, Pull
-import time
-import neopixel
 import board
-import pwmio
+import time 
+import math
+import pwmio 
 from adafruit_motor import servo
+from digitalio import DigitalInOut, Direction, Pull ## sets pin numbers and states for buttons
 btn = DigitalInOut(board.D3)
 btn2 = DigitalInOut(board.D2)
 btn.direction = Direction.INPUT
-btn.pull = Pull.UP
 btn2.direction = Direction.INPUT
+btn.pull = Pull.UP
 btn2.pull = Pull.UP
-# create a PWMOut object on Pin A2.
-pwm = pwmio.PWMOut(board.D3, duty_cycle=2 ** 15, frequency=50)
-# Create a servo object, my_servo.
-my_servo = servo.Servo(pwm)
 
+pwm = pwmio.PWMOut(board.D5, duty_cycle=2 **15, frequency=50)
+myServo = servo.Servo(pwm)
+
+print("starting")
 while True:
-    time.sleep(.1)
-    if btn == True:
-        for angle in range(0, 180, 90):  # 0 - 180 degrees, 100 degrees at a time.
-            my_servo.angle = angle
-    elif btn2 == True:
-        for angle in range(180, 0, -90): # 180 - 0 degrees, 100 degrees at a time.
-            my_servo.angle = angle
-    else:
-        print("click a button man")
+    print("re")
+    if btn.value == True: ## if button one is pressed turn 180 degrees
+        myServo.angle = 180
+        time.sleep(1)
+        print("Right")
+    elif btn2.value == True : ## if button 2 is pressed turn back to start position
+        myServo.angle = 0
+        time.sleep(1)
+        print("Left")
