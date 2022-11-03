f7ab6f80214f59aa711322f3961ecb8aac07c8d5
zsiller38 <zsiller38@charlottesvilleschools.org>
2022-11-03 15:46:00 -0400

    hi

---
+++ code/steppermotor.py
@@ -0,0 +1,16 @@
+# Zachary Siller
+# 11/13/2022
+#
+
+import board
+import time
+from analogio import AnalogOut, AnalogIn
+import simpleio
+
+motor = AnalogOut(board.A1)
+pot = AnalogIn(board.A0)
+
+while True:
+    print(simpleio.map_range(pot.value, 96, 65520, 0, 65535))
+    motor.value = int(simpleio.map_range(pot.value, 96, 65520, 0, 65535))
+    time.sleep(.1)
