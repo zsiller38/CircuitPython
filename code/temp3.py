d3f5d52149c061df75fa618e1838363db8819922
zsiller38 <zsiller38@charlottesvilleschools.org>
2023-03-23 15:35:58 -0400

    hi

---
+++ code/temp3.py
@@ -0,0 +1,35 @@
+import board   #[Lines 1-8] Importing all Neccessary libraries to communicate with LCD
+import time
+from lcd.lcd import LCD
+from lcd.i2c_pcf8574_interface import I2CPCF8574Interface
+from digitalio import DigitalInOut, Direction, Pull
+import board
+import analogio
+
+
+# get and i2c object
+i2c = board.I2C()
+tmp36 = analogio.AnalogIn(board.A0)
+# some LCDs are 0x3f... some are 0x27.
+lcd = LCD(I2CPCF8574Interface(i2c, 0x27), num_rows=2, num_cols=16)
+def tmp36_temperature_C(analogin):              #Convert millivolts to temperature
+    millivolts = analogin.value * (analogin.reference_voltage * 1000 / 65535)
+    return (millivolts - 500) / 10
+
+
+while True:
+    # Read the temperature in Celsius.
+    temp_C = tmp36_temperature_C(tmp36)  
+    # Convert to Fahrenheit.
+    temp_F = (temp_C * 9/5) + 32
+    # Print out the value and delay a second before looping again.
+    lcd.set_cursor_pos(0, 0)           #[Lines 26-36] Print different messages based on the temperature
+    if temp_F > 75:
+        lcd.print("it's too hot!")
+    elif temp_F < 70:
+        lcd.print("it's too cold")
+    else:
+        lcd.print("It's just right")
+    lcd.set_cursor_pos(1, 0)
+    lcd.print("Temp: {}F".format(temp_F))
+    time.sleep(.5) 
