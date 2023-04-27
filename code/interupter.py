7112ae29d9481781c4cb2a5d9ba5fb17b84eb369
zsiller38 <zsiller38@charlottesvilleschools.org>
2023-04-27 15:18:37 -0400

    hi good

--- code/interupter.py
+++ code/interupter.py
@@ -1,8 +1,11 @@
+# Zachary Siller
+# Engineering 3 3/28/2023
+# Count using a photointerupter
 import time
 import digitalio
 import board
 
-photoI = digitalio.DigitalInOut(board.D7)
+photoI = digitalio.DigitalInOut(board.D7) #sets photointerupter pin
 photoI.direction = digitalio.Direction.INPUT
 photoI.pull = digitalio.Pull.UP
 
@@ -13,9 +16,9 @@ photoICrosses = 0
 
 while True:
     if time.monotonic()-last_update > 4:
-        print(f"The number of crosses is {photoICrosses}")
+        print("The number of crosses is {photoICrosses}")
         last_update = time.monotonic()
 
-    if last_photoI != photoI.value and not photoI.value:
+    if last_photoI != photoI.value and not photoI.value: #checks the previous photo state and if they are different it adds one
         photoICrosses += 1
     last_photoI = photoI.value
