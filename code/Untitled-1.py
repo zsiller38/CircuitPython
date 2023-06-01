c66f0597a3f18dd620ec7ec48f1019c9db3a0975
zsiller38 <zsiller38@charlottesvilleschools.org>
2023-06-01 16:01:23 -0400

    hi

--- code/Untitled-1.py
+++ code/Untitled-1.py
@@ -8,6 +8,12 @@ import simpleio
 from lcd.lcd import LCD
 from lcd.i2c_pcf8574_interface import I2CPCF8574Interface
 from digitalio import DigitalInOut, Direction, Pull
+from PID_CPY import PID
+
+pid = PID(1, 0.1, 0.05, setpoint=1)
+pid.output_limits = (0, 65535)
+
+
 last_photoI = None #lines 11-13 start states for variables
 current_photoI = None
 lasttime=None
@@ -20,7 +26,7 @@ lcd.print("Motor Starting")
 rpm_list=[] #creates a list for all rpm values
 truetime=time.monotonic()
 while True:
-    
+    pid.setpoint = pot.value
     print(simpleio.map_range(pot.value, 96, 65520, 0, 65535)) #maps the motor value to the pot value
     motor.value = int(simpleio.map_range(pot.value, 96, 65520, 0, 65535)) 
     current_photoI = photoI.value #reads photo interupter value
@@ -39,11 +45,12 @@ while True:
             else:
                 average_rpm=current_rpm
             lcd.set_cursor_pos(0, 0) #lines 36-41 print average and current rpm on the LCD
-            lcd.print("RPM: {:.3f}     ".format(current_rpm)) #lcd.print("RPM: {}".format(int(current_rpm)))
+            lcd.print("RPM: {:.3f}".format(current_rpm)) #lcd.print("RPM: {}".format(int(current_rpm)))
+            print(current_rpm)
             lcd.set_cursor_pos(1, 0)
             lcd.print("AvgRPM: {:.3f}".format(average_rpm))
-            time.sleep(.1)
+            print(average_rpm)
+        last_photoI=current_photoI
         lasttime=time1 #makes the time1 the new lasttime
         print(lasttime, "hello")
-    last_photoI=current_photoI
             
